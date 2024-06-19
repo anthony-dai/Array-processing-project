c68af98868b75f545433fb5b1291d04199a80427
anthony-dai <74706762+anthony-dai@users.noreply.github.com>
2024-06-19 18:06:39 +0200

    updated project1.m

--- Project 1 Array parameter estimation/Project1.m
+++ Project 1 Array parameter estimation/Project1.m
@@ -82,26 +82,25 @@ m = 3;
 [estimated_theta, estimated_f] = joint(X,d,m);
 
 %% Comparison ESPRIT and Joint Diagonalization
+
 close all;
 clc;
 clear;
 
 % Parameters
-% Define parameters
-M = 10; % Number of antennas
+M = 3; % Number of antennas
 N = 20; % Number of samples
-d = 2; % Number of sources
 Delta = 0.5; % Antenna spacing in wavelengths
-theta = [10, -15]'; % Directions of the sources in degrees
-f = [0.3, 0.1]'; % Normalized frequencies of the sources
+theta = [-20, 30]'; % Directions of the sources in degrees
+f = [0.1, 0.12]'; % Normalized frequencies of the sources
 SNR_values = 0:4:20; % SNR values to test
 num_runs = 1000; % Number of test runs
 
 % Preallocate arrays to store results
-theta_esprit = zeros(num_runs, length(SNR_values), 2);
-freq_esprit = zeros(num_runs, length(SNR_values), 2);
-theta_joint = zeros(num_runs, length(SNR_values), 2);
-freq_joint = zeros(num_runs, length(SNR_values), 2);
+theta_esprit = zeros(num_runs, 2, length(SNR_values));
+freq_esprit = zeros(num_runs, 2, length(SNR_values));
+theta_joint = zeros(num_runs, 2, length(SNR_values));
+freq_joint = zeros(num_runs, 2, length(SNR_values));
 
 % Loop over SNR values
 for idx = 1:length(SNR_values)
@@ -110,46 +109,131 @@ for idx = 1:length(SNR_values)
         % Generate data
         [X, ~, ~] = gendata(M, N, Delta, theta, f, SNR);
 
-        % ESPRIT
-        theta_esprit(run, idx, :) = esprit(X, 2);
+        % ESPRIT for angles
+        theta_est = esprit(X, 2);
+        theta_esprit(run, :, idx) = theta_est(:);
 
-        % espritfreq
-        freq_esprit(run, idx, :) = espritfreq(X, 2);
+        % ESPRIT for frequencies
+        freq_est = espritfreq(X, 2);
+        freq_esprit(run, :, idx) = freq_est(:);
 
         % Joint Estimation
-        [theta_joint(run, idx, :), freq_joint(run, idx, :)] = joint(X, 2, 3);
+        [theta_joint_est, freq_joint_est] = joint(X, 2, 3);
+        theta_joint(run, :, idx) = theta_joint_est(:);
+        freq_joint(run, :, idx) = freq_joint_est(:);
     end
 end
 
-% Calculate mean and standard deviation for each algorithm
-mean_theta_esprit = mean(theta_esprit, 1);
-std_theta_esprit = std(theta_esprit, 0, 1);
-mean_freq_esprit = mean(freq_esprit, 1);
-std_freq_esprit = std(freq_esprit, 0, 1);
-mean_theta_joint = mean(theta_joint, 1);
-std_theta_joint = std(theta_joint, 0, 1);
-mean_freq_joint = mean(freq_joint, 1);
-std_freq_joint = std(freq_joint, 0, 1);
+% Average results over runs for each SNR value
+avg_theta_esprit = squeeze(mean(theta_esprit, 1));
+avg_freq_esprit = squeeze(mean(freq_esprit, 1));
+avg_theta_joint = squeeze(mean(theta_joint, 1));
+avg_freq_joint = squeeze(mean(freq_joint, 1));
 
-% Plotting
+% Plot results
 figure;
-subplot(2, 1, 1);
-errorbar(SNR_values, squeeze(mean_theta_esprit(:, :, 1)), squeeze(std_theta_esprit(:, :, 1)));
+subplot(2,1,1);
+plot(SNR_values, avg_theta_esprit(1, :), 'r', SNR_values, avg_theta_esprit(2, :), 'r--');
 hold on;
-errorbar(SNR_values, squeeze(mean_theta_joint(:, :, 1)), squeeze(std_theta_joint(:, :, 1)));
+plot(SNR_values, avg_theta_joint(1, :), 'b', SNR_values, avg_theta_joint(2, :), 'b--');
 xlabel('SNR (dB)');
-ylabel('Mean and STD of Estimated Angles (degrees)');
-legend('ESPRIT', 'Joint');
-title('Angle Estimation Performance');
+ylabel('Angle (degrees)');
+legend('ESPRIT Angle 1', 'ESPRIT Angle 2', 'Joint Angle 1', 'Joint Angle 2');
+title('Angle Estimation vs SNR');
 
-subplot(2, 1, 2);
-errorbar(SNR_values, squeeze(mean_freq_esprit(:, :, 1)), squeeze(std_freq_esprit(:, :, 1)));
+subplot(2,1,2);
+plot(SNR_values, avg_freq_esprit(1, :), 'r', SNR_values, avg_freq_esprit(2, :), 'r--');
 hold on;
-errorbar(SNR_values, squeeze(mean_freq_joint(:, :, 1)), squeeze(std_freq_joint(:, :, 1)));
+plot(SNR_values, avg_freq_joint(1, :), 'b', SNR_values, avg_freq_joint(2, :), 'b--');
 xlabel('SNR (dB)');
-ylabel('Mean and STD of Estimated Frequencies');
-legend('espritfreq', 'Joint');
-title('Frequency Estimation Performance');
+ylabel('Frequency');
+legend('ESPRIT Freq 1', 'ESPRIT Freq 2', 'Joint Freq 1', 'Joint Freq 2');
+title('Frequency Estimation vs SNR');
+
+
+%%
+
+% Generate data without noise
+%[X, A, S] = gendata(M, N, Delta, theta, f, 1000); % High SNR for no noise
+d = 2;
+[X, A, S] = gendata(M, N, Delta, theta, f, 10); % Low SNR for no noise
+%% Beamforming based on direction estimates
+% Estimate angles using ESPRIT
+theta_est_esprit = esprit(X, 2);
+
+% Construct steering matrix
+A_beam = zeros(M, d);
+for i = 1:d
+    phiZF_Angle = exp(1j*2*pi*Delta*sind(theta_est_esprit(i)));
+    for k = 0:M-1
+        A_beam(k+1, i) = phiZF_Angle^k;
+    end
+end
+
+% Compute beamforming weights
+W_esprit = (pinv(A_beam)).';
+
+% Recover sources
+S_esprit = W_esprit.' * X;
+
+%% Beamforming based on frequency estimates
+freq_est_freqesprit = espritfreq(X, 2);
+
+% Construct steering matrix S_hat based on frequency estimates
+S_hat = zeros(d, N); 
+for i = 1:d
+    phiZF_Freq = exp(1j*2*pi*freq_est_freqesprit(i));
+    for k = 0:N-1
+        S_hat(i, k+1) = phiZF_Freq^k;
+    end
+end
+
+A_hat = X * pinv(S_hat);
+W_espritfreq = pinv(A_hat);
+S_espritfreq = W_espritfreq * X;
+%%
+% Compute spatial responses
+theta_range = -90:1:90; % Angle range for plotting
+spatial_response_freq = zeros(2, length(theta_range));
+spatial_response_angle = zeros(2, length(theta_range));
+
+for idx = 1:length(theta_range)
+    theta = theta_range(idx);
+    a_theta = exp(1j*2*pi*Delta*(0:M-1)'*sind(theta)); % Steering vector
+
+    % Spatial response for angle-based beamformer
+    spatial_response_angle(1,idx) = norm(W_esprit(:,1)' * a_theta,2);
+    spatial_response_angle(2,idx) = norm(W_esprit(:,2)' * a_theta,2);
+    % Spatial response for frequency-based beamformer
+    spatial_response_freq(1,idx) = norm(W_espritfreq(2,:) * a_theta,2);
+    spatial_response_freq(2,idx) = norm(W_espritfreq(1,:) * a_theta,2);
+end
+
+% Plot the spatial responses
+figure;
+plot(theta_range, 20*log10(spatial_response_freq(1,:)), 'r', 'LineWidth', 2);
+hold on;
+plot(theta_range, 20*log10(spatial_response_angle(1,:)), 'b', 'LineWidth', 2);
+xlabel('Angle (degrees)');
+ylabel('Spatial Response (dB)');
+title('Spatial Responses of Zero-Forcing Beamformers');
+legend('Frequency-based Beamformer', 'Angle-based Beamformer');
+grid on;
+
+% Plot the spatial responses
+figure;
+plot(theta_range, 20*log10(spatial_response_freq(2,:)), 'r', 'LineWidth', 2);
+hold on;
+plot(theta_range, 20*log10(spatial_response_angle(2,:)), 'b', 'LineWidth', 2);
+xlabel('Angle (degrees)');
+ylabel('Spatial Response (dB)');
+title('Spatial Responses of Zero-Forcing Beamformers');
+legend('Frequency-based Beamformer', 'Angle-based Beamformer');
+grid on;
+
+
+% Display the plots
+hold off;
 %% Channel equalization
 close all;
 clc;
